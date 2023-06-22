2c0fd98977d752e0d693e0050f550a0c18e84a83
Anoiing <A534645655@outlook.com>
2023-06-22 11:51:57 +0800

    chore: 添加tailwindcss

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,6 +1,6 @@
 import './assets/style.css';
 import App from './App.svelte';
-import './assets/tailwind3.3.2';
+// import './assets/tailwind3.3.2';
 
 const app = new App({
   target: document.getElementById('app')
